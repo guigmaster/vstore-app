dbe285a84641cebc334b0f8f011e97faaa0e8081
Guilherme Silva <guisilva.master@gmail.com>
2018-06-22 17:30:40 -0300

    Add ProductCard component to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { ProductCardComponent } from '../components/product-card/product-card'
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
