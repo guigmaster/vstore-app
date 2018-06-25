5939a1d54ad782e00ed4e2b73fc413ba7b537f2b
Guilherme Silva <guisilva.master@gmail.com>
2018-06-25 15:32:13 -0300

    Generate a ProductServiceProvider and import HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 import { ProductCardComponent } from '../components/product-card/product-card'
+import { ProductServiceProvider } from '../providers/product-service/product-service';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,8 @@ import { ProductCardComponent } from '../components/product-card/product-card'
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,7 +31,8 @@ import { ProductCardComponent } from '../components/product-card/product-card'
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ProductServiceProvider
   ]
 })
 export class AppModule {}
