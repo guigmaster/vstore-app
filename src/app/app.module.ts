608f6fdc0a509a5cca097a10e5e1df7adee524bb
Guilherme Silva <guisilva.master@gmail.com>
2018-06-25 16:28:40 -0300

    Change application locale to pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 import { ProductCardComponent } from '../components/product-card/product-card'
 import { ProductServiceProvider } from '../providers/product-service/product-service';
+
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [
@@ -34,6 +38,7 @@ import { ProductServiceProvider } from '../providers/product-service/product-ser
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: LOCALE_ID, useValue: 'pt' },
     ProductServiceProvider
   ]
 })
